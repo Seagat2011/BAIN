a136d5ce434ec83b976a351eebe6aa36b7f876cf
Michelle Antonello <Seagat2011@users.noreply.github.com>
2019-06-15 22:40:01 -0400

    15.06.2019.22:39

--- ENGRIPPA.js
+++ ENGRIPPA.js
@@ -123,12 +123,379 @@ var __OPCODES__ = [
 ]
 */
 var __OPCODES__ = { '00' : true }//EOL//	
-function __ENGRIPPA__(arr){
-  this.__extend
-  this.__serialize
-  this.__deserialize
-  this.__decompile
-  this.__patch
-  this.__exec
+function __ENGRIPPA__(test_case) {
+
+    function __EGRIPP__() {
+        var self = this
+        self.lhs = []
+        self.rhs = []
+    }
+
+    // ---------------------------------------------------- LEXICAL ANALYSIS ---------------------------------------------------- //
+    var LEXEME = {
+        "'": 1,
+        '"': 1,
+        '[': 1,
+        ']': 1,
+        '{': 1,
+        '}': 1,
+        ':': 1,
+        ',': 1,
+        ' ': 1,
+    }
+    var lexbuff = new __EGRIPP__()
+    test_case.map((u,i)=>{
+        u.map((v,j)=>{
+            var b = ''
+            c = []
+            v.lhs.split('').map((w)=>{
+                if (w === ' ') {
+                    if (b) {
+                        c.push(b)
+                        b = ''
+                    }
+                } else if (w in LEXEME) {
+                    if (b) {
+                        c.push(b)
+                        b = ''
+                    }
+                    c.push(w)
+                } else {
+                    b += w
+                }
+            }
+            )
+            lexbuff.lhs.push(c)
+        }
+        )
+    }
+    )
+
+    // ---------------------------------------------------- SYNTAX ANALYSIS ---------------------------------------------------- //
+    Object.prototype.hasScope = function(u) {
+        return (u == this)
+    }
+    function __PSW__() {
+        var self = this
+        self.bracketstack = {
+            '{': 0,
+            '[': 0,
+        }
+        self.colonscope = false
+        self.tokenscope = {
+            scope: 'id',
+            id: 'id',
+            val: 'val'
+        }
+    }
+    var SYNTAX = {
+        "id": (u,i)=>{}
+        ,
+        "val": (u,i)=>{}
+        ,
+        "'": (u,i)=>{}
+        ,
+        '[': (u,i)=>{
+            var psw = SYNTAX.PSW
+            psw.bracketstack['[']++
+        }
+        ,
+        '{': (u,i)=>{
+            var psw = SYNTAX.PSW
+            psw.bracketstack['{']++
+        }
+        ,
+        '}': (u,i)=>{
+            var psw = SYNTAX.PSW
+            psw.bracketstack['{']--
+        }
+        ,
+        ']': (u,i)=>{
+            var psw = SYNTAX.PSW
+            psw.bracketstack['[']--
+        }
+        ,
+        '"': (u,i)=>{}
+        ,
+        ':': (u,i)=>{
+            SYNTAX.updateColonScope(true)
+        }
+        ,
+        ',': (u,i)=>{
+            var psw = SYNTAX.PSW
+            if ((psw.bracketstack['{'].hasScope(1) && psw.bracketstack['['].hasScope(0)) || (psw.bracketstack['{'].hasScope(0) && psw.bracketstack['['].hasScope(1))) {
+                SYNTAX.updateColonScope(false)
+            }
+        }
+        ,
+        ' ': (u,i)=>{}
+        ,
+        PSW: {},
+        updateColonScope: function(u) {
+            var psw = SYNTAX.PSW
+            if (u != null) {
+                if (u == true) {
+                    psw.colonscope = true
+                    psw.tokenscope.scope = psw.tokenscope.val
+                } else if (u == false) {
+                    psw.colonscope = false
+                    psw.tokenscope.scope = psw.tokenscope.id
+                }
+            } else {
+                psw.tokenscope.scope = psw.colonscope ? psw.tokenscope.val : psw.tokenscope.id
+            }
+        },
+    }
+    var synbuff = new __EGRIPP__()
+    lexbuff.lhs.map((w,i)=>{
+        SYNTAX.PSW = new __PSW__()
+        var c = []
+        w.map((u,j)=>{
+            var u_exists_in_lexeme = (u in SYNTAX)
+            if (u_exists_in_lexeme) {
+                SYNTAX[u](u)
+            } else {
+                u = SYNTAX.PSW.tokenscope.scope
+            }
+            c.push(u)
+            return u
+        }
+        )
+        synbuff.lhs.push(c)
+        return w
+    }
+    )
+
+    // ---------------------------------------------------- SEMANTIC ANALYSIS ---------------------------------------------------- //
+
+    var semabuff = new __EGRIPP__()
+    var GRAMMAR
+    function __GRAMMAR__() {
+        var self = this
+        self.lhs = []
+        self.rhs = []
+        self.lhs.idx = 0
+        self.rhs.idx = 0
+    }
+    __GRAMMAR__.prototype = []
+    var g_no_solution_found = true
+    function global_solution_monitor(e) {
+        if ('target'in e.data) {
+            var host = e.data
+            var status = host.unsolved_status
+            switch (host.target) {
+            case 'global-solution-update':
+                g_no_solution_found = status
+                break
+            }
+        }
+    }
+    addEventListener('message', global_solution_monitor, false)
+    Object.prototype.deep_parse = function(me, GRAMMAR, too) {
+        var self = this
+        if (g_no_solution_found) {
+            var src = too ? [...self, ...too] : [...self];
+            while (me[0] == src[0]) {
+                me.shift()
+                src.shift()
+            }
+            var scope_completed = ((me.length == 0) && (src.length == 0)) ? true : false;
+            var jmp_scope = (!scope_completed && src[0].match(/^_/)) ? src[0] : '';
+            var unsolved_solution = (g_no_solution_found == true)
+            var program_status_word = [scope_completed ? '1' : '0', jmp_scope ? '1' : '0', unsolved_solution ? '1' : '0', ].join('')
+            switch (program_status_word) {
+            case '000':
+                /* !scope_completed && !jmp_scope && !unsolved_solution */
+            case '010':
+                /* !scope_completed && jmp_scope && !unsolved_solution */
+            case '100':
+                /* scope_completed && !jmp_scope && !unsolved_solution */
+            case '110':
+                /* scope_completed && jmp_scope && !unsolved_solution */
+                // NOP //
+                break
+
+            case '001':
+                /* !scope_completed && !jmp_scope && unsolved_solution */
+                // NOP - console.info(`Semantic Parse Error: unexpected token ' ${me[0]} ' encountered - no solution found.`) //
+                break
+
+            case '011':
+                /* !scope_completed && jmp_scope && unsolved_solution */
+                var w = me[0]
+                src.shift()
+                !(jmp_scope in GRAMMAR) && self.assert(false, {
+                    msg: `Semantic Parse Error: unexpected token ' ${w} ' at index ???`
+                });
+                var j = GRAMMAR[jmp_scope].length - 1
+                var code = []
+                while (j + 1) {
+                    code.push(`GRAMMAR[jmp_scope][${j--}].deep_parse([...me],GRAMMAR,src)`)
+                }
+                code.length && eval(code.join('\n'))
+                break
+
+            case '101':
+                /* scope_completed && !jmp_scope && unsolved_solution */
+            case '111':
+                /* scope_completed && jmp_scope && unsolved_solution */
+                postMessage({
+                    target: 'global-solution-update',
+                    unsolved_status: false
+                })
+                break
+
+            default:
+                break
+            }
+        }
+    }
+    Object.prototype.parse = function(me, i) {
+        var self = this
+        var idx = i || 0
+        var w = me[idx]
+        var first_tok = {
+            '[': '_[_',
+            '{': '_{_'
+        };
+        ((idx == 0) && (w in first_tok)) || self.assert(false, {
+            msg: `Semantic Parse Error: unexpected token ' ${w} ' encountered at index ${idx}`
+        });
+        (idx == 0) && (w in first_tok) && (w = first_tok[w])
+        if (w in self) {
+            var code = []
+            var j = self[w].length - 1
+            while (j + 1) {
+                code.push(`self[w][${j--}].deep_parse(me,self)`)
+            }
+            eval(code.join('\n'))
+        } else {
+            self.assert(false, {
+                msg: `Semantic Parse Error: unexpected token ' ${w} ' at index ${idx}`
+            })
+        }
+        return (g_no_solution_found && true)
+    }
+    Object.prototype.assert = function(v, o) {
+        if (v == false) {
+            console.info(`Scan aborted:`)
+            throw Error(o.msg)
+        }
+    }
+    Object.prototype.add = function(u, j, k) {
+        var self = this;
+        (self.length < k + 1) && self.push({})
+        var src = self[k]
+        !(u in src) && (src[u] = {})
+        var srcu = src[u]
+        !(j in srcu) && (srcu[j] = 0)
+        srcu[j]++
+    }
+    Object.prototype.forEachOwnElement = function(cb) {
+        var self = this
+        var idx = 0
+        for (var u in self) {
+            if (self.hasOwnProperty(u)) {
+                cb && (u = cb(u, self[u], self, idx++))
+            }
+        }
+        return self
+    }
+    Object.prototype.next = function(w) {
+        var self = this
+        var idx = self.idx
+        var result = false
+        if (!self.tmp) {
+            var tmp = {}
+            self[0][w].forEachOwnElement((u,obj,me)=>{
+                tmp[u] = obj
+                return u
+            }
+            )
+            self.tmp = tmp
+        }
+        (w in self[idx]) || self.assert(false, {
+            msg: `Semantic Parse Error: unexpected token ' ${w} ' at index ${idx}`
+        })
+        var validStream = false
+        self[idx][w].forEachOwnElement((u,obj,me)=>{
+            (u in self.tmp) && (validStream = true)
+            return u
+        }
+        )
+        if (validStream) {
+            self.idx++
+            result = true
+        } else {
+            self.assert(false, {
+                msg: `Semantic Parse Error: unexpected token ' ${w} ' at index ${idx}`
+            })
+        }
+        return result
+    }
+    synbuff.lhs.map((w,idx,me)=>{
+        !GRAMMAR && (GRAMMAR = new __GRAMMAR__())
+        w.map((u,idy,too)=>{
+            GRAMMAR.lhs.add(u, idx, idy)
+            return u
+        }
+        )
+        return w
+    }
+    )
+
+    GRAMMAR.__exec = function(test) {
+        var self = this
+        var ii = 0
+        var II = test.length
+        var JJ = self.lhs.length;
+        (II <= JJ) || self.assert({
+            msg: `Semantic Parse Error: unexpected overall (extended) stream length. Implementation not available. Please update engrippa.`
+        })
+        while ((ii < II) && self.lhs.next(test[ii++]))
+            ;
+        if (ii == II) {
+            console.info('Semantic Engine - parse successfull.')
+        }
+    }
+    GRAMMAR.__decompile = function() {
+    }
+    GRAMMAR.__serialize = function() {
+    }
+    GRAMMAR.__deserialize = function() {
+    }
+    GRAMMAR.__patch = function() {
+    }
+    GRAMMAR.__extend = function() {
+    }
+    GRAMMAR.__includes = function() {
+    }
+
+    return GRAMMAR
+
 }
+// end __ENGRIPPA__
+
+var test_case = [[{
+    lhs: `['qid':qval,'qid':'qval','qid':qval]`,
+    rhs: `[ 'qid':qval ]`
+}], [{
+    lhs: `{ ' qid ' : qval , ' qid ' : ' qval ' , ' qid ' : qval }`,
+    rhs: `{ 'qid':qval }`
+}, {
+    lhs: `{ ' qid ' : qval , ' qid ' : ' qval ' , ' qid ' : qval , ' qid ' : ' qval ' , ' qid ' : qval }`,
+    rhs: `{ 'id':qval }`
+}, ], [{
+    lhs: `{ { ' qid ' , ' qid ' , { ' qid ' : ' qval ' } } : qval , ' qid ' : ' qval ' , { ' qid ' , ' qid ' , ' qid ' } : qval }`,
+    rhs: `{ id:{ 'id':qval } }`
+}], [{
+    lhs: `{ [ ' qid ' , ' qid ' , ' qid ' ] : qval , id : { ' qval ' , ' qval ' , ' qval ' } , [ ' qid ' , ' qid ' , ' qid ' ] : qval }`,
+    rhs: `{ 'id':qval }`
+}], ]
+
+var test = ["[", "'", "id", "'", ":", "val", ",", "'", "id", "'", ":", "'", "val", "'", ",", "'", "id", "'", ":", "val", "]"]
+
+var vm = new __ENGRIPPA__(test_case)
+vm.__exec(test)
+
 // INSTRUCTION_WORD : [OPCODE:8][DATA FIELD:N]
